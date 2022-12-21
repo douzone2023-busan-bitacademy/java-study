d54282494cf1bb50516c9f6bfaa1a33a2dcb35a7
douzone2023-busan-bitacademy <121003218+douzone2023-busan-bitacademy@users.noreply.github.com>
2022-12-21 16:34:01 +0900

    Update ArrayUtil.java

--- chapter03/src/main/java/chapter03/ArrayUtil.java
+++ chapter03/src/main/java/chapter03/ArrayUtil.java
@@ -2,14 +2,40 @@ package chapter03;
 
 public class ArrayUtil {
 
-	public static double[] intToDouble(int[] is) {
-		double[] result = new double[is.length];
+	public static double[] intToDouble(int[] a) {
+		double[] d = new double[a.length];
 		
-		for(int i = 0; i < is.length; i++) {
-			result[i] = is[i];
+		for(int i = 0; i < d.length; i++) {
+			d[i] = a[i];
 		}
 		
-		return result;
+		return d;
+	}
+
+	public static int[] doubleToInt(double[] d) {
+		int[] a = new int[d.length];
+		
+		for(int i = 0; i < a.length; i++) {
+			a[i] = (int)d[i];
+		}
+		
+		return a;
+	}
+
+	public static int[] concat(int[] a1, int[] a2) {
+		int[] a = new int[a1.length + a2.length];
+		
+		int index = 0;
+		
+		for(int i : a1) {
+			a[index++] = i;
+		}
+		
+		for(int i : a2) {
+			a[index++] = i;
+		}
+		
+		return a;
 	}
 
 }
