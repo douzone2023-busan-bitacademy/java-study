bfa0b296290e1f8ffec65d96522907a501c80310
douzone2023-busan-bitacademy <121003218+douzone2023-busan-bitacademy@users.noreply.github.com>
2022-12-21 19:24:22 +0900

    Add files via upload

--- practice02/src/main/java/prob01/Prob01.java
+++ practice02/src/main/java/prob01/Prob01.java
@@ -4,15 +4,20 @@ import java.util.Scanner;
 
 public class Prob01 {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner( System.in  );
+		Scanner scanner = new Scanner(System.in);
 
-		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
+		final int[] MONEYS = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1};
 
-		/* 코드 작성 */
-		System.out.print("금액 : ");
+		System.out.print("금액:");
 		int money = scanner.nextInt();
 
-		
+		for(int i = 0; i < MONEYS.length; i++) {
+			int count = money / MONEYS[i];
+			money -= (count * MONEYS[i]);
+
+			System.out.println(MONEYS[i] + "원 : " + count + "개");
+		}
+
 		scanner.close();
- 	}
+	}
 }
--- practice02/src/main/java/prob05/Prob05.java
+++ practice02/src/main/java/prob05/Prob05.java
@@ -6,40 +6,55 @@ import java.util.Scanner;
 public class Prob05 {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner( System.in );
-		while( true ) {
-			
-			/* 게임 작성 */
+		Scanner scanner = new Scanner(System.in);
+
+		while(true) {
+			// 게임 변수 초기화
+			int minNumber = 1;
+			int maxNumber = 100;
 
 			// 정답 램덤하게 만들기
 			Random random = new Random();
-			int correctNumber = random.nextInt(100) + 1;
-			System.out.println(correctNumber);
+			int correctNumber = random.nextInt(maxNumber) + minNumber;
+
+			// 게임 시작
+			int countTry = 0;
+			System.out.println("수를 결정하였습니다. 맞추어 보세요:");
 
 			while(true) {
-				System.out.println("1-100");
-				System.out.print("1>>");
-				int answer = scanner.nextInt();
-				
-				if(answer == correctNumber) {
-					System.out.println("맞았습니다.");
+				// 시도 회수 증가
+				countTry++;
+
+				// 추측 범위 출력
+				System.out.println(minNumber + "-" + maxNumber);
+				// 입력 프롬프트
+				System.out.print(countTry + ">>");
+
+				// 입력 받기
+				int guessNumber = scanner.nextInt();
+
+				// 정답
+				if(guessNumber == correctNumber) {
+					System.out.println("맞췄습니다.");
 					break;
 				}
-				
-				
-				
+
+				// 오답인 경우
+				if(guessNumber > correctNumber) {
+					System.out.println("더 낮게");
+					maxNumber = guessNumber;
+				} else {
+					System.out.println("더 높게");
+					minNumber = guessNumber;
+				}
 			}
-			
-			
-			//새 게임 여부 확인하기
-			System.out.print( "다시 하겠습니까(y/n)>>" );
+
+			System.out.print("다시 하시겠습니까?(y/n)>>");
 			String answer = scanner.next();
-			if( "y".equals( answer ) == false ) {
+			if("y".equals(answer.toLowerCase()) == false) {
 				break;
 			}
 		}
-		
 		scanner.close();
 	}
-
 }
