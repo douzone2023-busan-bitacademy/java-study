bb6c491713ee30cbc5561b257097b53f7fee77fc
douzone2023-busan-bitacademy <douzone2023.busan.bitacademy@gmail.com>
2022-12-21 16:31:06 +0900

    test

--- practice03/src/main/java/prob04/StringUtilTest.java
+++ practice03/src/main/java/prob04/StringUtilTest.java
@@ -7,6 +7,8 @@ public class StringUtilTest {
         String resultStr = StringUtil.concatenate( strArr );
 
         System.out.println( "결과 문자열 : " + resultStr ); 
+        System.out.println( "결과 문자열 : " + resultStr ); 
+        System.out.println( "결과 문자열 : " + resultStr ); 
 	}
 
 }
