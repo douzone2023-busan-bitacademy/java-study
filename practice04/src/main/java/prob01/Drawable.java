e16a545d85ec9eb372619117da0004c45c4445b3
douzone2023-busan-bitacademy <douzone2023.busan.bitacademy@gmail.com>
2022-12-26 09:54:31 +0900

    Generic Method

--- practice04/src/main/java/prob01/Drawable.java
+++
@@ -1,5 +0,0 @@
-package prob01;
-
-public interface Drawable {
-	public void draw();
-}
--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -2,19 +2,39 @@ package prob01;
 
 
 public class Printer {
-	public void println(int i) {
-		System.out.println(i);
+//	public void println(int i) {
+//		System.out.println(i);
+//	}
+//	
+//	public void println(boolean b) {
+//		System.out.println(b);
+//	}
+//
+//	public void println(double d) {
+//		System.out.println(d);
+//	}
+//
+//	public void println(String s) {
+//		System.out.println(s);
+//	}
+	
+	public <T> void println(T t) {
+		System.out.println(t);
 	}
 	
-	public void println(boolean b) {
-		System.out.println(b);
+	public int sum(Integer... nums) {
+		int s = 0;
+		for(Integer n:nums) {
+			s += n;
+		}
+		
+		return s;
 	}
-
-	public void println(double d) {
-		System.out.println(d);
+	
+	public <T> void println(T... ts) {
+		for(T t:ts) {
+			System.out.println(t);
+		}
 	}
-
-	public void println(String s) {
-		System.out.println(s);
-	}
+	
 }
