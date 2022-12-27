3eb930507a9497747961165b3bc8166e4bbaf218
douzone2023-busan-bitacademy <douzone2023.busan.bitacademy@gmail.com>
2022-12-27 09:51:04 +0900

    collection fw #3

--- chapter04/src/main/java/collection/HashSetTest01.java
+++ chapter04/src/main/java/collection/HashSetTest01.java
@@ -3,7 +3,7 @@ package collection;
 import java.util.HashSet;
 import java.util.Set;
 
-public class HashSetTest {
+public class HashSetTest01 {
 
 	public static void main(String[] args) {
 		Set<String> s = new HashSet<>();
