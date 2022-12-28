fc0183897964512d74fa8d0ebd97b062e7b6010b
douzone2023-busan-bitacademy <121003218+douzone2023-busan-bitacademy@users.noreply.github.com>
2022-12-28 17:13:14 +0900

    Update NSLookup.java

--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -2,15 +2,35 @@ package util;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class NSLookup {
 
 	public static void main(String[] args) {
-		try {
-			InetAddress[] inetAddresses = InetAddress.getAllByName("www.douzone.com");
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			try {
+				System.out.print(">>");
+				String line = scanner.nextLine();
+				
+				if("exit".equals(line)) {
+					break;
+				}
+				
+				InetAddress[] inetAddresses = 
+						InetAddress.getAllByName(line);
+				
+				for(InetAddress inetAddress : inetAddresses) {
+					System.out.println(line + ":" + inetAddress.getHostAddress());
+				}
+			} catch(UnknownHostException ex) {
+				System.out.println("unknown host");
+			}
+			
 		}
+		
+		scanner.close();
 	}
 
 }
+
