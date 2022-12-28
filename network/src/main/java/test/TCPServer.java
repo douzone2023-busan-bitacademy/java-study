c30f0511982e5d8931922f6deeb28db46ed53a92
douzone2023-busan-bitacademy <douzone2023.busan.bitacademy@gmail.com>
2022-12-28 15:30:07 +0900

    TCP C/S #finish

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -38,12 +38,17 @@ public class TCPServer {
 					byte[] buffer = new byte[256];
 					int readByteCount = is.read(buffer); // blocking
 					if(readByteCount == -1) {
+						// 클라이언트가 정상적으로 종료(close() 호출)
 						System.out.println("[server] closed by client");
 						break;
 					}
 					
 					String data = new String(buffer, 0, readByteCount, "utf-8");
 					System.out.println("[server] received:" + data);
+					
+					//6. 데이터 쓰기
+					
+					os.write(data.getBytes("utf-8"));
 				}
 			} catch(IOException ex) {
 				System.out.println("[server] error:" + ex);
