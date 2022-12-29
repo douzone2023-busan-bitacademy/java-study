23a8b49de8af5ada90b39f2f923e9f32d555fba1
douzone2023-busan-bitacademy <douzone2023.busan.bitacademy@gmail.com>
2022-12-29 15:31:09 +0900

    httpd #finish

--- network/src/main/java/httpd/RequestHandler.java
+++ network/src/main/java/httpd/RequestHandler.java
@@ -50,16 +50,15 @@ public class RequestHandler extends Thread {
 			}
 			
 			// 요청 처리
-			// consoleLog(request);
+			consoleLog(request);
+
 			String[] tokens = request.split(" ");
 			if("GET".equals(tokens[0])) {
-				consoleLog(request);
 				reponseStaticResource(outputStream, tokens[1], tokens[2]);
 			} else {
 				// methods: POST, PUT, DELETE, HEAD, CONNECT
 				// SimpleHttpServer 에서는 무시(400 Bad Resquest)
-				// 과제
-				// reponse400Error(outputStream, tokens[2]);
+				reponse400Error(outputStream, tokens[2]);
 			}
 			
 			
@@ -90,24 +89,43 @@ public class RequestHandler extends Thread {
 		OutputStream outputStream,
 		String url,
 		String protocol) throws IOException {
+		
 		// default(welcome) file set
 		if("/".equals(url)) {
 			url = "/index.html";
 		}
 		
 		File file = new File(DOCUMENT_ROOT + url);
+		if(!file.exists()) {
+			reponse404Error(outputStream, protocol);
+			return;
+		}
 		
 		// nio
 		byte[] body = Files.readAllBytes(file.toPath());
 		String contentType = Files.probeContentType(file.toPath());
 		
 		// 응답
-		outputStream.write("HTTP/1.1 200 OK\r\n".getBytes("UTF-8"));
+		outputStream.write((protocol + " 200 OK\r\n").getBytes("UTF-8"));
 		outputStream.write(("Content-Type:" + contentType + "; charset=utf-8\r\n").getBytes("UTF-8"));
 		outputStream.write("\r\n".getBytes());
 		outputStream.write(body);
 	}
 
+	private void reponse404Error(OutputStream outputStream, String protocol) {
+		// HTTP/1.1 404 Not Found
+		// Content-Type:.....
+		// \r\n
+		// .....
+	}
+
+	private void reponse400Error(OutputStream outputStream, String string) {
+		// HTTP/1.1 400 Bad Request
+		// Content-Type:.....
+		// \r\n
+		// .....
+	}
+	
 	public void consoleLog( String message ) {
 		System.out.println( "[httpd#" + getId() + "] " + message );
 	}
