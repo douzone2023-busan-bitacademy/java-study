767aa7bd14b90a2b2980cb697f4d6a203fc981a9
douzone2023-busan-bitacademy <douzone2023.busan.bitacademy@gmail.com>
2023-01-02 10:35:28 +0900

    chat client GUI #2

--- network/src/main/java/chat/gui/ChatWindow.java
+++ network/src/main/java/chat/gui/ChatWindow.java
@@ -41,6 +41,15 @@ public class ChatWindow {
 
 		// Textfield
 		textField.setColumns(80);
+		textField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				char keyCode = e.getKeyChar();
+				if(keyCode == KeyEvent.VK_ENTER) {
+					sendMessage();
+				}
+			}
+		});
 
 		// Pannel
 		pannel.setBackground(Color.LIGHT_GRAY);
@@ -53,9 +62,24 @@ public class ChatWindow {
 		frame.add(BorderLayout.CENTER, textArea);
 
 		// Frame
-		// frame.addWindowListener();
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				finish();
+			}
+		});
 		frame.setVisible(true);
 		frame.pack();
+		
+		// IOStream 받아오기
+		// ChatClientThread 생성하고 실행
+	}
+
+	private void finish() {
+		// quit protocol 구현
+		
+		// exit java(Application)
+		System.exit(0);
 	}
 	
 	private void sendMessage() {
@@ -64,5 +88,24 @@ public class ChatWindow {
 		
 		textField.setText("");
 		textField.requestFocus();
+		
+		// ChatClientThread 에서 서버로 부터 받은 메세지가 있다 치고~~~
+		updateTextArea("마이콜:" + message);
+	}
+	
+	private void updateTextArea(String message) {
+		textArea.append(message);
+		textArea.append("\n");
+	}
+	
+	private class ChatClientThread extends Thread {
+		@Override
+		public void run() {
+			// String message = br.readLine();
+			//
+			//
+			updateTextArea("안녕");
+		}
+		
 	}
 }
